55c0b38b4cc844ea790670c7d0bd0f3264550a64
Royaldeep1 <120001510+Royaldeep1@users.noreply.github.com>
2023-04-24 14:49:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -86,7 +86,7 @@ SPL_DELETE_TIME = int(environ.get('SPL_DELETE_TIME', 300))
 # URL SHORTNER
 
 URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'Atglinks.com')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '75c0f1777932058faefd8a8ee1659d3494660d28')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '83490976fb6e255155c11d54ac2b88b1f8834bb0')
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
